f52271e71f318c1b472301dc652e1e9dff7d580a
eeish <cheneiweei@gmail.com>
2022-10-19 18:57:57 +0800

    add ethers deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use ethers::{
+    prelude::*,
+    providers::{Provider, Ws},
+};
+use eyre::Result;
+use std::env;
+use std::sync::Arc;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+
+    let infura_url: String = env::var("INFURA_MAINNET_WS").unwrap();
+
+    let client = Provider::<Ws>::connect(infura_url).await?;
+    let client = Arc::new(client);
+
+    let mut stream = client.subscribe_pending_txs().await?;
+    while let Some(tx) = stream.next().await {
+        let tx = client.get_transaction(tx).await?.unwrap();
+        println!(
+            "Ts: {:?}, transaction from: {:?} -> {:?}",
+            tx.value, tx.from, tx.to
+        );
+    }
+    Ok(())
 }
