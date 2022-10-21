31e9d2abe7f2c21b5980b424aee478dd70e01897
eeish <cheneiweei@gmail.com>
2022-10-21 20:04:21 +0800

    handle read none from stream

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use ethers::{
 use eyre::Result;
 use std::env;
 use std::sync::Arc;
-use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -18,12 +17,19 @@ async fn main() -> Result<()> {
     let client = Arc::new(client);
 
     let mut stream = client.subscribe_pending_txs().await?;
+
     while let Some(tx) = stream.next().await {
-        let tx = client.get_transaction(tx).await?.unwrap();
-        println!(
-            "Ts: {:?}, transaction from: {:?} -> {:?}",
-            tx.value, tx.from, tx.to
-        );
+        let tx = client.get_transaction(tx).await?;
+
+        if tx.is_none() {
+            continue;
+        } else {
+            let tx = tx.unwrap();
+            println!(
+                "Ts: {:?}, transaction from: {:?} -> {:?}",
+                tx.value, tx.from, tx.to
+            );
+        }
     }
     Ok(())
 }
