9880dc1857117291177f802e38779877fd74eda1
eeish <cheneiweei@gmail.com>
2022-11-02 00:55:55 +0800

    fix: test_get_uni_pair_address

--- src/utils/univ2.rs
+++ src/utils/univ2.rs
@@ -1,4 +1,7 @@
+use ethers::abi::*;
 use ethers::prelude::*;
+use ethers::types::Address;
+use ethers::utils::keccak256;
 use hex;
 use sha3::{Digest, Keccak256};
 
@@ -22,12 +25,25 @@ pub fn get_uni_pair_address(from: Address, to: Address) -> Address {
         .unwrap();
 
     //// TODO remove hard code init code
-    let init_code =
+    let init_code_hash =
         hex::decode("96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f").unwrap();
 
-    let salt = create2_salt(address_pop(from.as_bytes()), address_pop(to.as_bytes()));
+    let mut extend_byte_array = from.as_bytes().to_vec();
+    let to_byte_array = to.as_bytes().to_vec();
+    extend_byte_array.extend(to_byte_array);
 
-    let pair_address = ethers::core::utils::get_create2_address(factory, salt, init_code.clone());
+    /// Attention here, ethers-rs: abi encoding not work
+    /// let input = abi::encode(&vec![Token::Address(from), Token::Address(to)]);
+    let salt = keccak256(&extend_byte_array);
+
+    let salt2 =
+        hex::decode("4aafb64a36177dc82e7ace74cf60cc655659bc049da9533b5f7a6881bea995c6").unwrap();
+
+    let pair_address = ethers::core::utils::get_create2_address_from_hash(
+        factory,
+        salt.to_vec(),
+        init_code_hash.to_vec(),
+    );
 
     pair_address
 }
@@ -54,4 +70,68 @@ fn create2_salt(token0: &[u8; 20], token1: &[u8; 20]) -> [u8; 32] {
 fn address_pop(addr: &[u8]) -> &[u8; 20] {
     addr.try_into()
         .expect("address raw bytes array length can't cast to 20")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ethers::abi::Token;
+    use ethers::abi::*;
+    use ethers::types::Address;
+    use ethers::utils::keccak256;
+    use std::str;
+
+    #[test]
+    fn test_sort_token() {
+        let token_usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+            .parse::<Address>()
+            .unwrap();
+        let token_usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+            .parse::<Address>()
+            .unwrap();
+        assert_eq!((token_usdc, token_usdt), sort_token(token_usdt, token_usdc));
+    }
+
+    //// #[test]
+    //// fn test_create2_salt_gen() {
+    ////     let token_usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+    ////         .parse::<ethers::types::Address>()
+    ////         .unwrap();
+    ////     let token_usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+    ////         .parse::<ethers::types::Address>()
+    ////         .unwrap();
+
+    ////     let input = abi::encode(&vec![
+    ////         Token::Address(token_usdt),
+    ////         Token::Address(token_usdc),
+    ////     ]);
+    ////     assert_eq!(
+    ////         "0xd4eb34f6fc5238007dd65d8d3f4e0ec192c1af0afab7ee25cd0d05284ddc2f48",
+    ////         keccak256(&input)
+    ////     );
+    //// }
+    #[test]
+    fn test_get_uni_pair_address() {
+        let token_usdt = "dAC17F958D2ee523a2206206994597C13D831ec7"
+            .parse::<Address>()
+            .unwrap();
+        let token_usdc = "A0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+            .parse::<Address>()
+            .unwrap();
+        assert_eq!(
+            "0x3041CbD36888bECc7bbCBc0045E3B1f144466f5f"
+                .parse::<Address>()
+                .unwrap(),
+            get_uni_pair_address(token_usdt, token_usdc)
+        );
+    }
+
+    //// #[test]
+    //// fn test_address_to_bytes() {
+    ////     let token_usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+    ////         .parse::<Address>()
+    ////         .unwrap();
+
+    ////     assert_eq!()
+    //// }
 }
